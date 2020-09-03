d05c5b918372dca83d58f4d4ed706c0c5fb6f4ae
satriaaprr99 <61263829+satriaaprr99@users.noreply.github.com>
2020-09-03 22:43:34 +0700

    Registrasi

---
+++ FilosofiWater/app/src/main/java/narsis/xiirpl2/filosofiwater/RegisterActivity.java
@@ -0,0 +1,105 @@
+package narsis.xiirpl2.filosofiwater;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private EditText nama, nohp, password, confirmpassword;
+    private Button btn_regist;
+    private ProgressBar loading;
+    private static String URL_REGIST = "http://192.168.1.14/AndroidFilosofiWater/register.php";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        loading = findViewById(R.id.loading);
+        nama = findViewById(R.id.nama);
+        nohp = findViewById(R.id.nohp);
+        password = findViewById(R.id.password);
+        confirmpassword = findViewById(R.id.confirmpassword);
+        btn_regist = findViewById(R.id.btn_regist);
+
+        btn_regist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Regist();
+            }
+        });
+    }
+
+    private void Regist(){
+        loading.setVisibility(View.VISIBLE);
+        btn_regist.setVisibility(View.GONE);
+
+        final String nama = this.nama.getText().toString().trim();
+        final String nohp = this.nohp.getText().toString().trim();
+        final String password = this.password.getText().toString().trim();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_REGIST,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                    try{
+                        JSONObject jsonObject = new JSONObject(response);
+                        String success = jsonObject.getString("success");
+                        if(success.equals("1")){
+                            Toast.makeText(RegisterActivity.this, "Registrasi Berhasil ", Toast.LENGTH_SHORT).show();
+                        }
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        Toast.makeText(RegisterActivity.this, "Registrasi Gagal " + e.toString(), Toast.LENGTH_SHORT).show();
+                        loading.setVisibility(View.GONE);
+                        btn_regist.setVisibility(View.VISIBLE);
+                    }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(RegisterActivity.this, "Registrasi Gagal " + error.toString(), Toast.LENGTH_SHORT).show();
+                        loading.setVisibility(View.GONE);
+                        btn_regist.setVisibility(View.VISIBLE);
+                    }
+                })
+
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("nama", nama);
+                params.put("nohp", nohp);
+                params.put("password", password);
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+
+    }
+
+}
